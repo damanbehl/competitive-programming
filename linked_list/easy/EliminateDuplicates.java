f9d7013ba93343b75dfdf8bd2bc0d6d973f24653
Daman Behl <damanbehl@gmail.com>
2022-02-16 16:59:37 -0500

    eliminate duplicates LL unsorted

---
+++ linked_list/easy/EliminateDuplicates.java
@@ -0,0 +1,31 @@
+package linked_list.easy;
+
+import java.util.HashMap;
+import linked_list.concept.Node;
+import linked_list.concept.LinkedListUse;
+
+public class EliminateDuplicates {
+    public static void main(String[] args) {
+        Node<Integer> head = LinkedListUse.takeInput();
+        LinkedListUse.print(head);
+        head = removeDuplicates(head);
+        LinkedListUse.print(head);
+    }
+
+    public static Node<Integer> removeDuplicates(Node<Integer> head) {
+
+        // Write your code here
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        Node<Integer> temp = head;
+        while (temp != null) {
+            hm.put(temp.data, 1);
+            if (temp.next != null && hm.get(temp.next.data) != null) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+
+        }
+        return head;
+    }
+}
