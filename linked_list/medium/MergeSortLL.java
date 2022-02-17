01b1509cfdee700aa9c1a7db0747f19950f12726
Daman Behl <damanbehl@gmail.com>
2022-02-17 17:11:38 -0500

    148. Sort List medium

--- linked_list/medium/MergeSortLL.java
+++ linked_list/medium/MergeSortLL.java
@@ -1,19 +1,44 @@
-package linked_list.easy;
+package linked_list.medium;
 
-//https://leetcode.com/problems/merge-two-sorted-lists/
-//https://www.codingninjas.com/codestudio/problems/merge-two-sorted-linked-lists_800332
+//https://leetcode.com/problems/sort-list
+//https://www.codingninjas.com/codestudio/problems/mergesort-linked-list_630514
 import linked_list.concept.LinkedListUse;
 import linked_list.concept.Node;
 
-public class MergeTwoSortedLL {
+public class MergeSortLL {
     public static void main(String[] args) {
         Node<Integer> head = LinkedListUse.takeInput();
-        Node<Integer> head2 = LinkedListUse.takeInput();
-        Node<Integer> result = sortTwoLists(head, head2);
+        Node<Integer> result = mergeSort(head);
         LinkedListUse.print(result);
     }
 
-    public static Node<Integer> sortTwoLists(Node<Integer> first, Node<Integer> second) {
+    public static Node<Integer> mergeSort(Node<Integer> head) {
+        // Write your code here.
+        if (head.next != null) {
+            Node<Integer> mid = findMid(head);
+            head = mergeSort(head);
+            mid = mergeSort(mid);
+            return merge(head, mid);
+        } else {
+            return head;
+        }
+    }
+
+    public static Node<Integer> findMid(Node<Integer> head) {
+        Node<Integer> slowPtr = head;
+        Node<Integer> fastPtr = head;
+        Node<Integer> prevSlowPtr = head;
+        while (fastPtr != null && fastPtr.next != null) {
+            prevSlowPtr = slowPtr;
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+        }
+        // snip snip
+        prevSlowPtr.next = null;
+        return slowPtr;
+    }
+
+    public static Node<Integer> merge(Node<Integer> first, Node<Integer> second) {
         // Write your code here.
         Node<Integer> newHead = null;
         Node<Integer> tail = null;
