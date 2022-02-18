35dc03e3ef0301194be78b0ba4133e13e6c31790
Daman Behl <damanbehl@gmail.com>
2022-02-17 19:42:00 -0500

    206. Reverse Linked List

---
+++ linked_list/easy/ReverseALL.java
@@ -0,0 +1,53 @@
+package linked_list.easy;
+//https://leetcode.com/problems/reverse-linked-list/
+import linked_list.concept.Node;
+
+import java.util.ArrayList;
+
+import linked_list.concept.LinkedListUse;
+
+public class ReverseALL {
+    Node<Integer> retHead = null;
+
+    public static void main(String[] args) {
+        Node<Integer> head = LinkedListUse.takeInput();
+        ReverseALL ob = new ReverseALL();
+        head = ob.reverseList(head);
+        LinkedListUse.print(head);
+    }
+
+    public Node<Integer> reverseList(Node<Integer> head) {
+        if (head == null || head.next == null) {
+            return head;
+        } else {
+            return reverseListHelper(head).get(0);
+        }
+    }
+
+    public ArrayList<Node<Integer>> reverseListHelper(Node<Integer> head) {
+        if (head.next == null) {
+            ArrayList<Node<Integer>> al = new ArrayList<Node<Integer>>();
+            al.add(head);
+            al.add(head);
+            return al;
+        } else {
+            Node<Integer> temp = head;
+            ArrayList<Node<Integer>> res = reverseListHelper(head.next);
+            Node<Integer> tail = res.get(1);
+            temp.next = null;
+            tail.next = temp;
+            tail = tail.next;
+            res.set(1, tail);
+            return res;
+        }
+    }
+
+    public void reverseListZeroMS(Node<Integer> head, Node<Integer> prevHead) {
+        if (head != null) {
+            reverseListZeroMS(head.next, head);
+            head.next = prevHead;
+        } else {
+            retHead = prevHead;
+        }
+    }
+}
