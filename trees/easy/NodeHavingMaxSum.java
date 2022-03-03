75c18aa994169a436b5ad750451128c1a274abf5
Daman Behl <damanbehl@gmail.com>
2022-03-03 15:39:51 -0500

    Node having sum of children and node is max

---
+++ trees/easy/NodeHavingMaxSum.java
@@ -0,0 +1,43 @@
+package trees.easy;
+
+import trees.concept.TreeNode;
+import trees.concept.TreeUseLevelWise;
+
+class Maximizer {
+    public TreeNode<Integer> node;
+    int sumOfselfandImmNodes;
+
+    public Maximizer(TreeNode<Integer> node, int sumOfselfandImmNodes) {
+        this.node = node;
+        this.sumOfselfandImmNodes = sumOfselfandImmNodes;
+    }
+}
+
+public class NodeHavingMaxSum {
+    public static void main(String[] args) {
+        TreeNode<Integer> root = TreeUseLevelWise.takeInput();
+        Maximizer ans = maxSumNode(root);
+        System.out.println(ans.sumOfselfandImmNodes);
+    }
+
+    public static Maximizer maxSumNode(TreeNode<Integer> root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.children.size() == 0) {
+            return new Maximizer(null, 0);
+        }
+        int sum = root.data;
+        for (TreeNode<Integer> node : root.children) {
+            sum += node.data;
+        }
+        Maximizer maxCurrentNode = new Maximizer(root, sum);
+        for (TreeNode<Integer> node : root.children) {
+            Maximizer maxFromChildren = maxSumNode(node);
+            if (maxFromChildren.sumOfselfandImmNodes > maxCurrentNode.sumOfselfandImmNodes) {
+                maxCurrentNode = maxFromChildren;
+            }
+        }
+        return maxCurrentNode;
+    }
+}
