384079f27fd7a5aef3160783d6d242f25aef4ae5
Daman Behl <damanbehl@gmail.com>
2022-03-03 19:25:45 -0500

    next greater element in n-ary tree

---
+++ trees/easy/NextLargerElement.java
@@ -0,0 +1,36 @@
+package trees.easy;
+
+import java.util.Scanner;
+
+import trees.concept.TreeNode;
+import trees.concept.TreeUseLevelWise;
+
+public class NextLargerElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        TreeNode<Integer> treeOne = TreeUseLevelWise.takeInput();
+        TreeNode<Integer> larger = nextLargerElement(treeOne, x);
+        if (larger != null) {
+            System.out.println(larger.data);
+        }
+    }
+
+    public static TreeNode<Integer> nextLargerElement(TreeNode<Integer> root, int x) {
+        TreeNode<Integer> currentNGR = null;
+        if (root.data > x) {
+            currentNGR = root;
+        }
+        for (int i = 0; i < root.children.size(); i++) {
+            TreeNode<Integer> smallAns = nextLargerElement(root.children.get(i), x);
+            if (smallAns != null) {
+                if (currentNGR != null && smallAns.data < currentNGR.data) {
+                    currentNGR = smallAns;
+                } else if (currentNGR == null) {
+                    currentNGR = smallAns;
+                }
+            }
+        }
+        return currentNGR;
+    }
+}
