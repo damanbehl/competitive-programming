d2b275170a73378052b979cc38cb0a840722b373
Daman Behl <damanbehl@gmail.com>
2022-03-05 02:16:11 -0500

    levelwise ops

--- binary_trees/concept/BinaryTreeUse.java
+++ binary_trees/concept/BinaryTreeUse.java
@@ -2,6 +2,9 @@ package binary_trees.concept;
 
 import java.util.Scanner;
 
+import exception.QueueEmptyException;
+import queues.concept.QueueUsingLL;
+
 public class BinaryTreeUse {
     public static void main(String[] args) {
         // BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(2);
@@ -11,8 +14,11 @@ public class BinaryTreeUse {
         // root.right = right;
         // System.out.println(" empty");
         Scanner sc = new Scanner(System.in);
-        BinaryTreeNode<Integer> root = takeInput(sc);
+        // BinaryTreeNode<Integer> root = takeInput(sc);
+        BinaryTreeNode<Integer> root = takeInputLevelWise();
         printTree(root);
+        System.out.println("Printing levelwise");
+        printLevelWise(root);
     }
 
     public static BinaryTreeNode<Integer> takeInput(Scanner sc) {
@@ -41,5 +47,60 @@ public class BinaryTreeUse {
         System.out.println(print);
         printTree(root.left);
         printTree(root.right);
+    }
+
+    public static BinaryTreeNode<Integer> takeInputLevelWise() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter root data");
+        int rootData = sc.nextInt();
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+        QueueUsingLL<BinaryTreeNode<Integer>> pendingNodes = new QueueUsingLL<>();
+        pendingNodes.enqueue(root);
+        while (!pendingNodes.empty()) {
+            BinaryTreeNode<Integer> front;
+            try {
+                front = pendingNodes.dequeue();
+            } catch (QueueEmptyException e) {
+                return null;
+            }
+            System.out.println("Enter left child of " + front.data);
+            int leftData = sc.nextInt();
+            if (leftData != -1) {
+                BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftData);
+                pendingNodes.enqueue(leftChild);
+                front.left = leftChild;
+            }
+            System.out.println("Enter right child of " + front.data);
+            int rightData = sc.nextInt();
+            if (rightData != -1) {
+                BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightData);
+                pendingNodes.enqueue(rightChild);
+                front.right = rightChild;
+            }
+        }
+        return root;
+    }
+
+    public static void printLevelWise(BinaryTreeNode<Integer> root) {
+        QueueUsingLL<BinaryTreeNode<Integer>> pendingNodes = new QueueUsingLL<>();
+        pendingNodes.enqueue(root);
+        while (!pendingNodes.empty()) {
+            BinaryTreeNode<Integer> front;
+            try {
+                front = pendingNodes.dequeue();
+            } catch (QueueEmptyException e) {
+                return;
+            }
+            String current = front.data + " :";
+            if (front.left != null) {
+                pendingNodes.enqueue(front.left);
+                current += "L>" + front.left.data + ", ";
+            }
+            if (front.right != null) {
+                pendingNodes.enqueue(front.right);
+                current += "R>" + front.right.data + ", ";
+            }
+            System.out.println(current);
+        }
     }
 }
