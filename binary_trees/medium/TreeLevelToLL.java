94b4bd25001d292a66d2cc3c8de0141e02434cc1
Daman Behl <damanbehl@gmail.com>
2022-03-10 03:38:08 -0500

    116. Populating Next Right Pointers in Each Node

---
+++ binary_trees/medium/TreeLevelToLL.java
@@ -0,0 +1,56 @@
+package binary_trees.medium;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+
+public class TreeLevelToLL {
+    public static void main(String[] args) {
+
+    }
+
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> pendingNodes = new LinkedList<Node>();
+        pendingNodes.add(root);
+        while (!pendingNodes.isEmpty()) {
+            Node front = pendingNodes.peek();
+            int n = pendingNodes.size();
+            for (int i = 0; i < n; i++) {
+                front = pendingNodes.poll();
+                if (front == null) {
+                    continue;
+                }
+                front.next = pendingNodes.peek();
+                pendingNodes.add(front.left);
+                pendingNodes.add(front.right);
+            }
+            if (front != null) {
+                front.next = null;
+            }
+        }
+        return root;
+    }
+}
