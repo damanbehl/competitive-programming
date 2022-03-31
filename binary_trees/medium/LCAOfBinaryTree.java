a64d33f9585a335fffc2d47d4fa2973a137de839
Daman Behl <damanbehl@gmail.com>
2022-03-31 18:53:35 -0400

    LCA first intuition

---
+++ binary_trees/medium/LCAOfBinaryTree.java
@@ -0,0 +1,62 @@
+package binary_trees.medium;
+
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        return getLCA(root, p, q);
+    }
+
+    public static TreeNode getLCA(TreeNode root, TreeNode p,
+            TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        return getLCAHelper(root, p, q).node;
+    }
+
+    public static Helper getLCAHelper(TreeNode root,
+            TreeNode p,
+            TreeNode q) {
+        Helper res = new Helper(false, false, null, 0);
+        if (root == null) {
+            return res;
+        }
+        if (root.val == p.val || root.val == q.val) {
+            if (root.val == p.val) {
+                res.pres = true;
+            } else {
+                res.qres = true;
+            }
+        }
+        Helper leftAns = getLCAHelper(root.left, p, q);
+        Helper rightAns = getLCAHelper(root.right, p, q);
+        res.pres = res.pres || leftAns.pres || rightAns.pres;
+        res.qres = res.qres || leftAns.qres || rightAns.qres;
+        res.height = Math.max(leftAns.height, rightAns.height) + 1;
+        res.node = root;
+        if ((leftAns.pres && leftAns.qres) && (rightAns.pres && rightAns.qres)) {
+            return leftAns.height < rightAns.height ? leftAns : rightAns;
+        } else if (leftAns.pres && leftAns.qres) {
+            return leftAns;
+        } else if (rightAns.pres && rightAns.qres) {
+            return rightAns;
+        }
+        return res;
+
+    }
+}
+
+class Helper {
+    boolean pres;
+    boolean qres;
+    TreeNode node;
+    int height;
+
+    public Helper(boolean pres, boolean qres, TreeNode node, int height) {
+        this.pres = pres;
+        this.qres = qres;
+        this.node = node;
+        this.height = 0;
+    }
+}
--- utility/Pair.java
+++ utility/Pair.java
@@ -3,4 +3,9 @@ package utility;
 public class Pair<T, V> {
     public T first;
     public V second;
+
+    public Pair(T first, V second) {
+        this.first = first;
+        this.second = second;
+    }
 }
