0068a0141222a31eb08ba436601ea90b57978810
Daman Behl <damanbehl@gmail.com>
2022-04-25 19:09:29 -0400

    Largest subarray with 0 sum

---
+++ map/easy/MaxSubArrayZeroSum.java
@@ -0,0 +1,36 @@
+package map.easy;
+
+//https://practice.geeksforgeeks.org/problems/largest-subarray-with-0-sum
+import java.util.HashMap;
+
+public class MaxSubArrayZeroSum {
+    public static void main(String[] args) {
+        int input[] = { 1, -1, 2, -2 };
+        System.out.println(maxLen(input, input.length));
+    }
+
+    // first sum 0 is on -1 index
+    // occurence is map for <sum:index>
+    // track previously occurred sums on repitition find out the stretch of elements ,
+    // that sum to 0
+    static int maxLen(int arr[], int n) {
+        // Your code here
+        HashMap<Integer, Integer> occurrence = new HashMap<>();
+        occurrence.put(0, -1);
+        int sumT = 0;
+        int maxSubArray = 0;
+        for (int i = 0; i < n; i++) {
+            int x = arr[i];
+            sumT += x;
+            if (occurrence.containsKey(sumT)) {
+                int foundAt = occurrence.get(sumT);
+                if (i - foundAt > maxSubArray) {
+                    maxSubArray = i - foundAt;
+                }
+            } else {
+                occurrence.put(sumT, i);
+            }
+        }
+        return maxSubArray;
+    }
+}
--- map/medium/UniqKDiffPairs.java
+++ map/medium/UniqKDiffPairs.java
@@ -30,4 +30,33 @@ public class UniqKDiffPairs {
         }
         return result;
     }
+
+    // with rep
+    // https://www.codingninjas.com/codestudio/problems/pair-with-diff-k_5393
+    public static int getPairsWithDifferenceK(int nums[], int k) {
+        if (k < 0) {
+            return 0;
+        }
+        int result = 0;
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int x : nums) {
+            freq.put(x, freq.getOrDefault(x, 0) + 1);
+        }
+        for (int x : nums) {
+            int xcount = freq.get(x);
+            if (k == 0) {
+                if (xcount > 1) {
+                    freq.put(x, xcount - 1);
+                    result = result + xcount - 1;
+                }
+            } else {
+                int ccount = freq.getOrDefault(x + k, 0);
+                if (ccount > 0) {
+                    freq.put(x, xcount - 1);
+                    result += ccount;
+                }
+            }
+        }
+        return result;
+    }
 }
