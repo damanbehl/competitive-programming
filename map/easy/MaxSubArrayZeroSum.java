280b477bd3da548ca22862e1f556cf4971519892
Daman Behl <damanbehl@gmail.com>
2022-04-30 02:04:40 -0400

    Min Priority Queue using heap

--- map/easy/MaxSubArrayZeroSum.java
+++ map/easy/MaxSubArrayZeroSum.java
@@ -5,13 +5,14 @@ import java.util.HashMap;
 
 public class MaxSubArrayZeroSum {
     public static void main(String[] args) {
-        int input[] = { 1, -1, 2, -2 };
+        int input[] = { 95, -97, -387, -435, -5, -70, 897, 127, 23, 284 };
         System.out.println(maxLen(input, input.length));
     }
 
     // first sum 0 is on -1 index
     // occurence is map for <sum:index>
-    // track previously occurred sums on repitition find out the stretch of elements ,
+    // track previously occurred sums on repitition find out the stretch of elements
+    // ,
     // that sum to 0
     static int maxLen(int arr[], int n) {
         // Your code here
---
+++ priority_queue/Priority_Queue.java
@@ -0,0 +1,91 @@
+package priority_queue;
+
+import java.util.ArrayList;
+
+import exception.PriorityQueueException;
+
+public class Priority_Queue {
+    private ArrayList<Integer> heap;
+
+    public Priority_Queue() {
+        heap = new ArrayList<>();
+    }
+
+    boolean isEmpty() {
+        return heap.size() == 0;
+    }
+
+    int size() {
+        return heap.size();
+    }
+
+    int getMin() throws PriorityQueueException {
+        if (isEmpty()) {
+            throw new PriorityQueueException();
+
+        }
+        return heap.get(0);
+    }
+
+    void insert(int element) {
+        // up-heapify
+        heap.add(element);
+        int childIndex = heap.size() - 1;
+        int parentIndex = (childIndex - 1) / 2;
+        while (childIndex > 0) {
+            if (heap.get(childIndex) < heap.get(parentIndex)) {
+                int temp = heap.get(parentIndex);
+                heap.set(parentIndex, heap.get(childIndex));
+                heap.set(childIndex, temp);
+                childIndex = parentIndex;
+                parentIndex = (childIndex - 1) / 2;
+            } else {
+                return;
+            }
+        }
+    }
+
+    int removeMin() throws PriorityQueueException {
+        if (isEmpty()) {
+            PriorityQueueException e = new PriorityQueueException();
+            throw e;
+        }
+        int min = heap.get(0);
+        heap.set(0, heap.get(size() - 1));
+        heap.remove(size() - 1);
+        int parentIndex = 0;
+        int lChildIndex = 2 * parentIndex + 1;
+        int rChildIndex = 2 * parentIndex + 2;
+        // down -heapify
+        while (parentIndex < size() - 1) {
+            lChildIndex = 2 * parentIndex + 1;
+            rChildIndex = 2 * parentIndex + 2;
+            int leftChildVal = Integer.MAX_VALUE;
+            int rightChildVal = Integer.MAX_VALUE;
+            if (lChildIndex <= size() - 1) {
+                leftChildVal = heap.get(lChildIndex);
+            }
+            if (rChildIndex <= size() - 1) {
+                rightChildVal = heap.get(rChildIndex);
+            }
+            int parentVal = heap.get(parentIndex);
+            int minVal = Math.min(leftChildVal, Math.min(rightChildVal, parentVal));
+            if (minVal < parentVal) {
+                // repindex: index to be replaced
+                int repIndex;
+                if (leftChildVal == minVal) {
+                    repIndex = lChildIndex;
+                } else {
+                    repIndex = rChildIndex;
+                }
+                int temp = heap.get(repIndex);
+                heap.set(repIndex, parentVal);
+                heap.set(parentIndex, temp);
+                parentIndex = repIndex;
+            } else {
+                return min;
+            }
+        }
+        return min;
+    }
+}
